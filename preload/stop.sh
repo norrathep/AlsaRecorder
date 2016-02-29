d6718eb8e6eda9baa11cc230e406971df2d9c78e
Oak <nrattana@uci.edu>
2016-02-29 15:55:57 -0800

    add start/stop script

---
+++ preload/stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ ! -f out.raw ]; then
+    exit
+fi
+rm start.oak
+if [ -f start.oak ]; then
+    rm start.oak
+fi
+
+
+sox -r 44100 -b 32 -e signed-integer -c 2 out.raw out.wav 
+
+rm out.raw
